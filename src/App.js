9b55e5fa6832705bbc8ac569b7f370cfcbe9e306
skavik <tomniukj@gmail.com>
2022-05-31 23:08:18 +0300

    refactoring App

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import { Cart, Home } from "./pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
+import store from "./redux/store";
+import { setPizzas } from "./redux/actions/pizzas";
 
-function App() {
+
+function App(props) {
   React.useEffect(() => {
     axios.get("http://localhost:3000/db.json").then(({ data }) => {
-      setPizzas(data.pizzas);
+      store.dispatch(setPizzas(data.pizzas));
     });
   }, []);
 
@@ -20,7 +23,7 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home items={Pizzas} />} exact />
+            <Route path="/" element={<Home items={props.items} />} exact />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
@@ -29,6 +32,16 @@ function App() {
   );
 }
 
-const mapStateToProps = (state) => {};
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+  };
+};
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => {
+  return {
+    setPizzas: items => dispatch(setPizzas(items))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
